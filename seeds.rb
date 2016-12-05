7e499695e48303c950bb5a89b677205ab90ecf96
Guy Roberts <guy@MacBook-Pro.local>
2016-12-05 19:23:24 +0000

    Try smaller files

--- seeds.rb
+++ seeds.rb
@@ -2,7 +2,7 @@ require 'Faker'
 
 File.open('example_data.csv', 'w') do |f|
 
-  10000.times do 
+  400.times do 
    f.write "#{Faker::Name.name}, #{Faker::Internet.email}, #{Faker::Address.postcode}, #{Faker::Crypto.md5}\n"
   end
 
