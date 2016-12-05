8c58b89ce7a5dcdaf9bd9a6800e5315f10b111e3
Guy Roberts <guy@MacBook-Pro.local>
2016-12-05 19:33:37 +0000

    Even smaller - trying to see the cvs diff working

--- seeds.rb
+++ seeds.rb
@@ -2,7 +2,7 @@ require 'Faker'
 
 File.open('example_data.csv', 'w') do |f|
 
-  400.times do 
+  40.times do 
    f.write "#{Faker::Name.name}, #{Faker::Internet.email}, #{Faker::Address.postcode}, #{Faker::Crypto.md5}\n"
   end
 
